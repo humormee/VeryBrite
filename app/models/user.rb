2ca1f67eb8a225b2aeda17a780ad391f30c4453e
Justin Ernst <jernst766@gmail.com>
2021-07-03 14:31:59 -0500

    added users model auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,44 @@
 class User < ApplicationRecord
-  
+  validates :first_name, :last_name, :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true}
+  validates :username, :email, :session_token, presence: true, uniqueness: true
+
+  attr_reader :password
+
+  # has_many :events,
+  # foreign_key: :author_id,
+  # class_name: "Event"
+
+  # has_many :registrations,
+  # foreign_key :attendee_id,
+  # class_name "Registration"
+
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+
+    return nul if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.base64(64)
+    self.save!
+    self.session_token
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= SecureRandom.base64(64)
+  end
 end
