c82af7e563e09d471494c5e4e6506b3818a00fb6
Justin Ernst <jernst766@gmail.com>
2021-07-03 14:48:04 -0500

    working on backend controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,12 @@
 class SessionsController < ApplicationController
+  # before_action :require_signed_in!, only: [:destroy]
+
+  # def new
+  #   render :json
+  # end
+
+  # def create
+  # end
+
+  # def
 end
