6701464f503f14d93e0de335db6c984b01a4b5e3
Justin Ernst <jernst766@gmail.com>
2021-07-13 17:20:12 -0500

    initial create form styling

--- frontend/components/events/create_event.jsx
+++ frontend/components/events/create_event.jsx
@@ -44,37 +44,58 @@ class CreateEvent extends React.Component {
     
     return (
       <div className="create-form-container">
-        <div>
-          <form onSubmit={e => this.handleSubmit(e)} className="create-form">
+          <form onSubmit={e => this.handleSubmit(e)}  className="create-form">
             <div className="event-create-details">
-              <h3>Title</h3>
-              <input className="event-create-title" type="text"
-              value={title}
-              onChange={e => this.update(e, 'title')}/>
 
+              <div className="title-container">
+                <h3>Title</h3>
+                <div className="title">
+                  <i class="fas fa-align-justify fa-3x"></i>
+                  <input className="event-create-title" type="text"
+                  value={title}
+                  onChange={e => this.update(e, 'title')}/>
+                </div>
+              </div>
 
-              <h3>When should the event start?</h3>
-              <input id="start-date" type="datetime-local"
-              value={start_time}
-              onChange={e => this.update(e, 'start_time')}/>
-              <h3>When should the event end?</h3>
-            
-              <input id="end-date" type="datetime-local"
-              value={end_time}
-              onChange={e => this.update(e, 'end_time')}/>
-              
-              <h3>Give a description of the event:</h3>
-              <div>
-                <i class="far fa-sticky-note"></i>
-                <textarea value={description}
-                cols="30" rows="10" onChange={e => this.update(e, 'description')}></textarea>
+              <div className="start-date-container">
+                <h3>When should the event start?</h3>
+                <div className="start-date">
+                  <i className="far fa-calendar-alt fa-3x"></i>
+                  <input id="start-date" type="datetime-local"
+                  value={start_time}
+                  onChange={e => this.update(e, 'start_time')}/>
+                </div>
               </div>
               
+              <div className="end-date-container">
+                <h3>When should the event end?</h3>
+                <div className="end-date">
+                  <i className="far fa-calendar-alt fa-3x"></i>
+                  <input id="end-date" type="datetime-local"
+                  value={end_time}
+                  onChange={e => this.update(e, 'end_time')}/>
+                </div>
+              </div>
+            
               
+              
+              <div className="description-container">
+                <h3>Give a description of the event:</h3>
+                <div className="description">
+                  <i class="far fa-sticky-note fa-3x"></i>
+                  <textarea value={description}
+                  cols="30" rows="10" onChange={e => this.update(e, 'description')}></textarea>
+                </div>
+              </div>
+
+              <div>
+                
             </div>
-            <button type="submit">Create</button>
-          </form>
-        </div>
+              
+              
+          </div>
+          {/* <button type="submit">Create</button> */}
+        </form>
       </div>
     )
   }
