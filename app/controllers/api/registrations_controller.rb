0659a071e0b42691a295a887ba1249b7dbf0fddd
Justin Ernst <jernst766@gmail.com>
2021-07-15 09:29:51 -0500

    remove registration modal for presentation

--- app/controllers/api/registrations_controller.rb
+++ app/controllers/api/registrations_controller.rb
@@ -35,7 +35,7 @@ class Api::RegistrationsController < ApplicationController
   end
 
   def destroy
-    debugger
+    # debugger
     @registration = Registration.find(params[:id])
     if @registration && @registration.destroy
       render json: ["Registration deleted"]
