1dacff7d987200db3a03ee4bd031ae9b523ba604
Justin Ernst <jernst766@gmail.com>
2021-08-30 15:55:55 -0500

    add errors to edit form

--- frontend/components/events/event_edit.jsx
+++ frontend/components/events/event_edit.jsx
@@ -6,8 +6,10 @@ class EventEdit extends React.Component {
   constructor(props) {
     super(props);
 
+    debugger
     if (props.event) {
-      this.state = props.event
+      debugger
+      this.state = Object.assign(props.event, {current_time: ""})
     }
     
 
@@ -27,15 +29,88 @@ class EventEdit extends React.Component {
             category: event.category,
             description: event.description,
             start_time: event.start_time,
-            end_time: event.end_time
+            end_time: event.end_time,
+            current_time: this.getCurrentTime()
           })
         })
+        // this.setState({current_time: this.getCurrentTime()})
   }
   
+
+    getCurrentTime() {
+      let today = new Date();
+      let min = today.getMinutes();
+      let hh = today.getHours();
+      let dd = today.getDate();
+      let mm = today.getMonth()+1;
+      let yyyy = today.getFullYear();
+      if(min<10){
+        min='0'+min
+      } 
+      if(hh<10){
+        hh='0'+hh
+      } 
+      if(dd<10){
+        dd='0'+dd
+      } 
+      if(mm<10){
+        mm='0'+mm
+      } 
+      return yyyy+'-'+mm+'-'+dd+'T'+hh+ ":"+min;
+    }
+
+  renderErrors(field) {
+    if(!this.state) {
+      return
+    }
+    switch (field) {
+      case 'title':
+        if(this.state.title.length >= 40) {
+          return (
+            <p className="title-errors">titles must be under 40 characters</p>
+          );
+        } else {
+          return null
+        }
+      case 'start_time':
+        if(this.state.start_time < this.getCurrentTime() && this.state.start_time.length > 0) {
+          return (
+            <p className="date-errors">Start time must be after the current time</p>
+          );
+        } else {
+          return null
+        }
+        
+      case 'end_time':
+        if(this.state.start_time > this.state.end_time && this.state.end_time.length > 0) {
+          return (
+            <p className="date-errors">Start time must be before end time</p>
+          );
+        } else {
+          return null
+        }
+      default:
+        break;
+    }
+  }
+
+  isErrors() {
+    if(this.renderErrors('title') === null && this.renderErrors('start_time') === null && this.renderErrors('end_time') === null) {
+      debugger
+      return false;
+    } else {
+      debugger
+      return true;
+    }
+  }
 
   handleSubmit(e){
 
     e.preventDefault();
+
+     if(this.isErrors()) {
+      return
+    }
     this.props.updateEvent(this.state).then((event) => {
       this.props.history.push(`/events/${event.event.id}`)
     })
@@ -51,6 +126,13 @@ class EventEdit extends React.Component {
  
 
   update(e, field){
+    if(field === 'title' && e.currentTarget.value.length > 40) {
+      return
+    } else if(field === 'end_time' && e.currentTarget.value < this.state.start_time) {
+      return (
+        <p className="date-errors">Start time must be before end time</p>
+      )
+    }
     this.setState({ [field]: e.currentTarget.value})
   }
 
@@ -74,6 +156,7 @@ class EventEdit extends React.Component {
                   <i class="fas fa-align-justify fa-3x"></i>
                   <h3>Title</h3>
                 </div>
+                <div>{this.renderErrors('title')}</div>
                   <input className="event-edit-title" type="text" value={title}
                   onChange={e => this.update(e, 'title')}/>
               </div>
@@ -100,9 +183,11 @@ class EventEdit extends React.Component {
                   <i className="far fa-calendar-alt fa-3x"></i>
                   <h3>Date and time</h3>
                 </div>
+                <div>{this.renderErrors('start_time')}</div>
                 <p>start date and time (click the right icon for calendar view)</p>
                 
                   <input id="start-date" type="datetime-local" value={start_time.slice(0,-1)} onChange={e => this.handleStartDate(e)}/>
+                  <div>{this.renderErrors('end_time')}</div>
                   <p>end date and time (click the right icon for calendar view)</p>
                   <input id="end-date" type="datetime-local" value={end_time.slice(0, -1)} onChange={e => this.handleEndDate(e)}/>
                 </div>
