6bb50c7ffd95a4eea66e9703d35b80b9528723ba
Justin Ernst <jernst766@gmail.com>
2021-08-30 15:42:14 -0500

    add input errors create form

--- frontend/components/events/create_event.jsx
+++ frontend/components/events/create_event.jsx
@@ -9,7 +9,8 @@ class CreateEvent extends React.Component {
       title: "",
       description: "",
       start_time: "",
-      end_time: ""
+      end_time: "",
+      current_time: ""
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -17,16 +18,90 @@ class CreateEvent extends React.Component {
 
   componentDidMount(){
     this.setState({author_id: this.props.userId})
+    this.setState({current_time: this.getCurrentTime()})
+  }
+
+  getCurrentTime() {
+    let today = new Date();
+    let min = today.getMinutes();
+    let hh = today.getHours();
+    let dd = today.getDate();
+    let mm = today.getMonth()+1; //January is 0 so need to add 1 to make it 1!
+    let yyyy = today.getFullYear();
+    if(min<10){
+      min='0'+min
+    } 
+    if(hh<10){
+      hh='0'+hh
+    } 
+    if(dd<10){
+      dd='0'+dd
+    } 
+    if(mm<10){
+      mm='0'+mm
+    } 
+    return yyyy+'-'+mm+'-'+dd+'T'+hh+ ":"+min;
+  }
+
+  renderErrors(field) {
+    switch (field) {
+      case 'title':
+        if(this.state.title.length >= 40) {
+          return (
+            <p className="title-errors">titles must be under 40 characters</p>
+          );
+        } else {
+          return null
+        }
+      case 'start_time':
+        if(this.state.start_time < this.getCurrentTime() && this.state.start_time.length > 0) {
+          return (
+            <p className="date-errors">Start time must be after the current time</p>
+          );
+        } else {
+          return null
+        }
+        
+      case 'end_time':
+        if(this.state.start_time > this.state.end_time && this.state.end_time.length > 0) {
+          return (
+            <p className="date-errors">Start time must be before end time</p>
+          );
+        } else {
+          return null
+        }
+      default:
+        break;
+    }
+  }
+
+  isErrors() {
+    if(this.renderErrors('title') === null && this.renderErrors('start_time') === null && this.renderErrors('end_time') === null) {
+      debugger
+      return false;
+    } else {
+      debugger
+      return true;
+    }
   }
 
   handleSubmit(e) {
     e.preventDefault();
+    if(this.isErrors()) {
+      return
+    }
     this.setState({author_id: this.props.userId});
-    this.setState({category: "Free"});
     this.props.createEvent(this.state).then(event => this.props.history.push(`/events/${event.event.id}`));
   }
 
   update(e, field) {
+    if(field === 'title' && e.currentTarget.value.length > 40) {
+      return
+    } else if(field === 'end_time' && e.currentTarget.value < this.state.start_time) {
+      return (
+        <p className="date-errors">Start time must be before end time</p>
+      )
+    }
     this.setState({ [field]: e.currentTarget.value });
   }
 
@@ -37,16 +112,9 @@ class CreateEvent extends React.Component {
     if (!this.state) {
       return null;
     }
-    // CATEGORIES
-    // 'Music'
-    // 'Free'
-    // 'Entertainment'
-    // "Charity and Causes"
-    // 'Food and Drink'
-    // 'Other'
     
+
     const { title, start_time, end_time, category, description }  = this.state;
-    
     return (
       <div className="create-form-container">
           <form onSubmit={e => this.handleSubmit(e)}  className="create-form">
@@ -56,6 +124,7 @@ class CreateEvent extends React.Component {
                 <div className="title">
                 <i class="fas fa-align-justify fa-3x"></i>
                 <h3>Title</h3>
+                <div>{this.renderErrors('title')}</div>
                 </div>
                 <input className="event-create-title" 
                 type="text" placeholder="enter a title"
@@ -79,25 +148,26 @@ class CreateEvent extends React.Component {
                     <option value="Other">Other</option>
                   </select>
                </div>
-                 {/* <div className="genre-description-index">
-                  <select value={category} onChange={e => this.update(e, 'category')}>
 
-                  </select>
-                 </div> */}
-              
 
               <div className="start-date-container">
                 <div className="start-date">
                 <i className="far fa-calendar-alt fa-3x"></i>
                 <h3>Date and time</h3>
                 </div>
+                <div>{this.renderErrors('start_time')}</div>
                 <p>Start day and time (click right icon for calendar view)</p>
-                  <input id="start-date" type="datetime-local"
+                  <input id="start-date" 
+                  type="datetime-local"
+                  min={this.getCurrentTime()}
                   value={start_time}
                   onChange={e => this.update(e, 'start_time')}/>
                   <br />
+                  <div>{this.renderErrors('end_time')}</div>
                   <p>End day and time (click right icon for calendar view)</p>
-                  <input id="end-date" type="datetime-local"
+                  <input id="end-date" 
+                  type="datetime-local"
+                  min={this.state.start_time}
                   value={end_time}
                   onChange={e => this.update(e, 'end_time')}/>
               </div>
