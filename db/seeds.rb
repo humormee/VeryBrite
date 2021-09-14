d21030822559e268c872fb02534be0c1f5de6650
Justin Ernst <jernst766@gmail.com>
2021-09-14 16:39:04 -0500

    style event items

--- db/seeds.rb
+++ db/seeds.rb
@@ -66,7 +66,8 @@ event1 = Event.create!(
   author_id: user2.id,
   category: 'Food and Drink',
   title: 'Picnic',
-  description: "picnic in Pioneers Park",
+  description: "Come by for some treats in the park. Pioneers park is an iconic staple of the city where old, young and everyone in between can come together outdoor get togethers.
+",
   start_time: "2021-07-28 11:00:00",
   end_time: "2021-07-28 14:00:00",
   image_url: "https://images.unsplash.com/photo-1592753054398-9fa298d40e85?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGljbmljfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
@@ -76,7 +77,8 @@ event2 = Event.create!(
   author_id: user1.id,
   category: 'Entertainment',
   title: 'Movie Night',
-  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed libero varius, faucibus ipsum vel, malesuada turpis. Nam congue felis vitae eleifend hendrerit. Mauris ultrices, neque vel maximus efficitur, felis leo consectetur ligula, maximus interdum arcu nibh eget massa. In non mi porta, iaculis magna ut, aliquam mi.",
+  description: "Snap. Crackle. Pop some (pop)corn. We also have movies.
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed libero varius, faucibus ipsum vel, malesuada turpis. Nam congue felis vitae eleifend hendrerit. Mauris ultrices, neque vel maximus efficitur, felis leo consectetur ligula, maximus interdum arcu nibh eget massa. In non mi porta, iaculis magna ut, aliquam mi.",
   start_time: "2021-08-18 20:00:00",
   end_time: "2021-07-28 23:00:00",
   image_url: "https://images.unsplash.com/photo-1585647347483-22b66260dfff?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
@@ -86,7 +88,9 @@ event3 = Event.create!(
   author_id: demo.id,
   category: 'Music',
   title: 'Rave the Cave',
-  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed libero varius, faucibus ipsum vel, malesuada turpis. Nam congue felis vitae eleifend hendrerit. Mauris ultrices, neque vel maximus efficitur, felis leo consectetur ligula, maximus interdum arcu nibh eget massa. In non mi porta, iaculis magna ut, aliquam mi. ",
+  description: "Getting tired of man-made concert venues? well, now’s your chance to rave where we’re supposed to rave: in a cave, obviously.
+  
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed libero varius, faucibus ipsum vel, malesuada turpis. Nam congue felis vitae eleifend hendrerit. Mauris ultrices, neque vel maximus efficitur, felis leo consectetur ligula, maximus interdum arcu nibh eget massa. In non mi porta, iaculis magna ut, aliquam mi. ",
   start_time: "2021-08-09 22:00:00",
   end_time: "2021-08-10 01:00:00",
   image_url: "https://images.unsplash.com/photo-1555086156-e6c7353d283f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmF2ZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
@@ -95,8 +99,10 @@ event3 = Event.create!(
 event4 = Event.create!(
   author_id: user4.id,
   category: 'Charity and Causes',
-  title: 'Lincoln Helping Hands',
-  description: "Nam elementum mollis nibh, et interdum eros aliquet vitae. Nullam suscipit magna quam, eu consectetur eros fermentum at. Nulla diam magna, sagittis vel lacinia ac, suscipit et odio.",
+  title: 'Habitat for Humanity',
+  description: "Build a church while building friendships! Prior experience with construction is required. If you can, bring your own tools.
+  
+  Nam elementum mollis nibh, et interdum eros aliquet vitae. Nullam suscipit magna quam, eu consectetur eros fermentum at. Nulla diam magna, sagittis vel lacinia ac, suscipit et odio.",
   start_time: "2021-09-18 15:00:00",
   end_time: "2021-09-18 17:00:00",
   image_url: "https://images.unsplash.com/photo-1469571486292-0ba58a3f068b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8aGVscGluZyUyMGhhbmRzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
@@ -105,8 +111,10 @@ event4 = Event.create!(
 event5 = Event.create!(
   author_id: user5.id,
   category: 'Entertainment',
-  title: 'Harry Potter Potion Class',
-  description: "Mauris ultrices, neque vel maximus efficitur, felis leo consectetur ligula, maximus interdum arcu nibh eget massa. In non mi porta, iaculis magna ut, aliquam mi. Nam elementum mollis nibh, et interdum eros aliquet vitae. Nullam suscipit magna quam, eu consectetur eros fermentum at.",
+  title: 'Potion Making',
+  description: "Create some magic in this potion making class. This event is meant for beginners, although expert wizards and witches are welcome.
+  
+  Mauris ultrices, neque vel maximus efficitur, felis leo consectetur ligula, maximus interdum arcu nibh eget massa. In non mi porta, iaculis magna ut, aliquam mi. Nam elementum mollis nibh, et interdum eros aliquet vitae. Nullam suscipit magna quam, eu consectetur eros fermentum at.",
   start_time: "2021-08-20 20:00:00",
   end_time: "2021-08-20 23:00:00",
   image_url: "https://images.unsplash.com/photo-1551269901-5c5e14c25df7?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fG1hZ2ljfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
@@ -116,7 +124,9 @@ event6 = Event.create!(
   author_id: user5.id,
   category: 'Food and Drink',
   title: 'Yoga with Alpacas',
-  description: " Nulla diam magna, sagittis vel lacinia ac, suscipit et odio. Praesent ornare ex sed lectus commodo, in tristique sem sodales. Integer erat quam, auctor vitae lacus eu, suscipit commodo odio. Pellentesque nec leo sit amet nibh commodo pellentesque imperdiet eu massa. Morbi vel blandit sem. Integer ac venenatis orci.",
+  description: "Just like yoga with goats, only with alpacas.
+
+   Nulla diam magna, sagittis vel lacinia ac, suscipit et odio. Praesent ornare ex sed lectus commodo, in tristique sem sodales. Integer erat quam, auctor vitae lacus eu, suscipit commodo odio. Pellentesque nec leo sit amet nibh commodo pellentesque imperdiet eu massa. Morbi vel blandit sem. Integer ac venenatis orci.",
   start_time: "2021-08-04 8:00:00",
   end_time: "2021-07-28 9:30:00",
   image_url: " https://images.unsplash.com/photo-1577795894605-4a77189b7bc7?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YWxwYWNhfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
@@ -126,7 +136,7 @@ event7 = Event.create!(
   author_id: user6.id,
   category: 'Food and Drink',
   title: 'Margs and Args',
-  description: "In non mi porta, iaculis magna ut, aliquam mi. Nam elementum mollis nibh, et interdum eros aliquet vitae. Nullam suscipit magna quam, eu consectetur eros fermentum at. Nulla diam magna, sagittis vel lacinia ac, suscipit et odio. Praesent ornare ex sed lectus commodo, in tristique sem sodales. Integer erat quam, auctor vitae lacus eu, suscipit commodo odio. Pellentesque nec leo sit amet nibh commodo pellentesque imperdiet eu massa.",
+  description: "So you’ve been keeping up with the news and now you have an opinion. Except your friends are getting tired of listening to your politics. Margs and Args is a safe place for expressing your opinions to strangers while having a drink!",
   start_time: "2021-08-18 17:00:00",
   end_time: "2021-08-28 20:00:00",
   image_url: "https://images.unsplash.com/photo-1628873268744-c30c97e17917?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzJ8fG1hcmdhcml0YXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
@@ -136,7 +146,7 @@ event8 = Event.create!(
   author_id: user5.id,
   category: 'Charity and Causes',
   title: 'Bars for Mars',
-  description: "Nulla diam magna, sagittis vel lacinia ac, suscipit et odio. Praesent ornare ex sed lectus commodo, in tristique sem sodales. Integer erat quam, auctor vitae lacus eu, suscipit commodo odio. Pellentesque nec leo sit amet nibh commodo pellentesque imperdiet eu massa.",
+  description: "Look, Earth is great and all, but it’s time for humans to become the next interplanetary species. Bars for Mars is a NASA fundraiser where local artists freestyle rap to spacey beats.",
   start_time: "2021-08-25 11:00:00",
   end_time: "2021-08-25 2:00:00",
   image_url: "https://images.unsplash.com/photo-1601643157091-ce5c665179ab?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmFwfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
