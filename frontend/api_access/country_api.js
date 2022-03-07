5a89bdd144f8c325ad0ac48ac20cfc82a8bdb5ae
Justin Ernst <jernst766@gmail.com>
2022-03-06 20:53:50 -0600

    add token requests

---
+++ frontend/api_access/country_api.js
@@ -0,0 +1,7 @@
+let req = unirest("GET", "https://www.universal-tutorial.com/api/getaccesstoken");
+
+req.headers({
+  "Accept": "application/json",
+  "api-token": "LIs_DXLSbK44MngqLOKVJvLYH5i0QDqooxFtvfPBkkcZo6SRN4U78RYl0-_wSEs8bP4",
+  "user-email": "jernst766@gmail.com"
+})
